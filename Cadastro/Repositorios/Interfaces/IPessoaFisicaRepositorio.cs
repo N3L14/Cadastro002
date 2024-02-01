ddf9e99e4023920e18c25bea26d08dac73e3bb29
Nélia Simas <manasbrodas@gmail.com>
2024-02-01 18:15:40 -0300

    up

---
+++ Cadastro/Repositorios/Interfaces/IPessoaFisicaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Cadastro.Models;
+
+namespace Cadastro.Repositorios.Interfaces
+{
+    public interface IPessoaFisicaRepositorio
+    {
+        Task<List<PessoaFisicaModel>> BuscarTodasPessoas();
+        Task<PessoaFisicaModel> BuscarId();
+        Task<PessoaFisicaModel> Adicionar(PessoaFisicaModel pessoaFisica);
+        Task<PessoaFisicaModel> Atualizar(PessoaFisicaModel pessoaFisica, int Id);
+        Task<bool> Deletar(int Id);
+    }
+}
